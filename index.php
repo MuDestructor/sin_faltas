fd92173421347834f7e71af96134682269555907
Fergus Reig Gracia <fergusrg@gmail.com>
2021-01-12 20:34:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ $startURL = "";
 //When no $startURL is configured above, miniProxy will show its own landing page with a URL form field
 //and the configured example URL. The example URL appears in the instructional text on the miniProxy landing page,
 //and is proxied when pressing the 'Proxy It!' button on the landing page if its URL form is left blank.
-$landingExampleURL = "https://dle.rae.es/zanahoria";
+$landingExampleURL = "https://www.eldiario.es/sociedad/espana-registra-dato-diario-contagios-pandemia_1_6746127.html";
 
 /****************************** END CONFIGURATION ******************************/
 
